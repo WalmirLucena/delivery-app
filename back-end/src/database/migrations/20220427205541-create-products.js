7893b879959df9595e65515f8072c69e2fc1d498
WalmirLucena <walmirlucena3@gmail.com>
2022-04-27 17:06:11 -0400

    feat: adding Products migrations, models and seeder

---
+++ back-end/src/database/migrations/20220427205541-create-products.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.NUMBER,
+        allowNull: false,
+      },
+      url_image: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      }
+    });
+  },
+  
+  down: async(queryInterface) => dropTable('products'),
+ 
+};
