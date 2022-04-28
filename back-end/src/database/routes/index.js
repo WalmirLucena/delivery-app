59b9aea0328504e8a2aa0f83f3328b4a2a9edb2e
WalmirLucena <walmirlucena3@gmail.com>
2022-04-28 08:32:30 -0400

    fix: Importing userRoute on index.js

--- back-end/src/database/routes/index.js
+++ back-end/src/database/routes/index.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
+const userRoute = require('./userRoutes')
 
 const router = Router();
 
-router.use('/login', userRouter);
+router.use('/login', userRoute);
 
 module.exports = router;
