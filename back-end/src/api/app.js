9e7f1a51c9c811696871018421afcabf5e74c94f
WalmirLucena <walmirlucena3@gmail.com>
2022-04-28 08:30:18 -0400

    feat: Adding route on /Api

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const router = require('../database/routes');
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.get('/coffee', (_req, res) => res.status(418).end());
+app.use('/', router);
 
 module.exports = app;
