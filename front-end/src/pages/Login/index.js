19fbca126d4fd265a1e9c5a6f11d723e1c7d8ccf
Augusto <augusto.raminelli>
2022-04-29 16:14:16 -0300

    feat: create function validateEmail and validatePassword

--- front-end/src/pages/Login/index.js
+++ front-end/src/pages/Login/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LoginInput from './LoginInput';
 
 import logo from '../../images/logo.png';
 import './Login.css';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const validateEmail = () => {
+    const re = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+    return re.test(String(email).toLowerCase());
+  };
+
+  const validatePassword = () => {
+    const MIN_PASSWORD = 6;
+    return password.length > MIN_PASSWORD;
+  };
+
   return (
     <div className="login-background">
       <img alt="logo" className="logo" src={ logo } />
@@ -12,17 +25,17 @@ function Login() {
         <LoginInput
           name="email"
           onChange={ ({ target }) => setEmail(target.value) }
-          // value={ email }
+          value={ email }
         />
         <LoginInput
           name="password"
           onChange={ ({ target }) => setPassword(target.value) }
-          // value={ password }
+          value={ password }
         />
         <button
           className="login-button"
           data-testid="login-submit-btn"
-          // disabled={ !validateEmail() || !validatePassword() }
+          disabled={ !validateEmail() || !validatePassword() }
           type="submit"
         >
           LOGIN
