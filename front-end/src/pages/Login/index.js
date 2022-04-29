96a63d06f0aea2971654ec548ca4c7ff70fecade
Augusto <augusto.raminelli>
2022-04-29 16:04:45 -0300

    created Login page and file Login.css

--- front-end/src/pages/Login/index.js
+++ front-end/src/pages/Login/index.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import LoginInput from './LoginInput';
+
+import logo from '../../images/logo.png';
+import './Login.css';
 
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="login-background">
+      <img alt="logo" className="logo" src={ logo } />
+      <form className="login-form">
+        <LoginInput
+          name="email"
+          onChange={ ({ target }) => setEmail(target.value) }
+          // value={ email }
+        />
+        <LoginInput
+          name="password"
+          onChange={ ({ target }) => setPassword(target.value) }
+          // value={ password }
+        />
+        <button
+          className="login-button"
+          data-testid="login-submit-btn"
+          // disabled={ !validateEmail() || !validatePassword() }
+          type="submit"
+        >
+          LOGIN
+        </button>
+        <button
+          className="register-button"
+          data-testid="login-submit-btn"
+          // disabled={ !validateEmail() || !validatePassword() }
+          type="submit"
+        >
+          Ainda não tenho conta
+        </button>
+      </form>
     </div>
   );
 }
