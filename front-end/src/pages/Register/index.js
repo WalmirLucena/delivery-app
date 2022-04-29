62e4411711e2ec197d98c7d958e8004bfd8e4d8a
Augusto <augusto.raminelli>
2022-04-29 17:11:23 -0300

    feat: created page Register, RegisterInput and Register CSS

--- front-end/src/pages/Register/index.js
+++ front-end/src/pages/Register/index.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import RegisterInput from './RegisterInput';
 
 function Register() {
   return (
-    <div>
-      <h1>Register</h1>
+    <div className="login-background">
+      <h1>Faça seu Cadastro!</h1>
+      <form className="register-form">
+        <RegisterInput
+          name="name"
+          onChange={ ({ target }) => setName(target.value) }
+          placeholder="Seu Nome"
+          // value={ name }
+        />
+        <RegisterInput
+          name="email"
+          onChange={ ({ target }) => setEmail(target.value) }
+        // value={ email }
+        />
+        <RegisterInput
+          name="password"
+          onChange={ ({ target }) => setPassword(target.value) }
+          // value={ password }
+        />
+        <button
+          className="register-button"
+          data-testid="login-submit-btn"
+          // disabled={ !validateEmail() || !validatePassword() }
+          type="submit"
+        >
+          CADASTRAR
+        </button>
+      </form>
     </div>
   );
 }
