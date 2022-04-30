305c4429dddecfe3125600367c83fd88283fa0e2
Augusto <augusto.raminelli>
2022-04-30 15:50:37 -0300

    fix: fixed component ProductCard

--- front-end/src/components/ProductCard.js
+++ front-end/src/components/ProductCard.js
@@ -3,18 +3,22 @@ import PropTypes from 'prop-types';
 
 function ProductCard({ price, name, image }) {
   return (
-    <div className="product-card">
-      <div className="product-card-price">
-        <h3>{ price }</h3>
+    <>
+      <div className="product-card">
+        <div className="product-card-price">
+          <h3>{ price }</h3>
+        </div>
+        <div className="product-card-image">
+          <img src={ image } alt={ name } />
+        </div>
       </div>
-      <div className="product-card-image">
-        <img src={ image } alt={ name } />
+      <div>
+        <div className="product-card-name">
+          <h3>{ name }</h3>
+        </div>
+        {/* input addCart */}
       </div>
-      <div className="product-card-name">
-        <h3>{ name }</h3>
-      </div>
-      {/* input addCart */}
-    </div>
+    </>
   );
 }
 
