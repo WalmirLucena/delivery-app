8071f6c30074a25bdc889c83de93aa2fd24044cd
Augusto <augusto.raminelli>
2022-04-30 15:13:37 -0300

    feat: created Header component and css file

--- front-end/src/components/NavBarCustomer.js
+++ front-end/src/components/NavBarCustomer.js
@@ -1,37 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../images/logo.png';
 import '../styles/NavBarCustomer.css';
+import Header from './Header';
 
 function NavBarCustomer() {
   return (
-    <nav>
-      <div className="header">
-        <img alt="Logo Delivery" className="logo" src={ logo } />
-        <span className="user-name">
-          Usuário
-        </span>
-      </div>
-      <div className="links">
-        <Link
-          className={ window.location.pathname === '/customer/products' ? 'active' : '' }
-          to="/customer/products"
-        >
-          Produtos
-        </Link>
-        <Link
-          className={ window.location.pathname === '/customer/orders' ? 'active' : '' }
-          to="/customer/orders"
-        >
-          Meus Pedidos
-        </Link>
-        <Link
-          to="/login"
-        >
-          Sair
-        </Link>
-      </div>
-    </nav>
+    <>
+      <Header />
+      <nav>
+        <div className="links">
+          <Link
+            className={
+              window.location.pathname === '/customer/products' ? 'active' : ''
+            }
+            to="/customer/products"
+          >
+            Produtos
+          </Link>
+          <Link
+            className={ window.location.pathname === '/customer/orders' ? 'active' : '' }
+            to="/customer/orders"
+          >
+            Meus Pedidos
+          </Link>
+          <Link
+            to="/login"
+          >
+            Sair
+          </Link>
+        </div>
+      </nav>
+    </>
   );
 }
 
