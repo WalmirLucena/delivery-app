f83d85031386debb992cea3aeda46646e9791e69
Augusto <augusto.raminelli>
2022-05-01 20:18:16 -0300

    feat: created UserList Component

--- front-end/src/pages/AdminManage/index.js
+++ front-end/src/pages/AdminManage/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBarAdmin from '../../components/NavBarAdmin';
 import UserRegister from '../../components/RegisterUser';
+import UserList from '../../components/UserList';
 
 function AdminManage() {
   return (
     <>
       <NavBarAdmin />
       <UserRegister />
+      <UserList />
     </>
   );
 }
