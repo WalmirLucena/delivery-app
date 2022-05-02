1c8677cf8ea7c22098655cd3fd92f7b7f2a0ac6c
Thiago Oshiro <oshirothiago@gmail.com>
2022-05-02 19:14:51 -0300

    fix: cors implemented

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { routes } = require('../routes');
-
+const cors = require('cors');
+ 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 routes.forEach((route) => {
   const { method, path, middleware, controller, 
