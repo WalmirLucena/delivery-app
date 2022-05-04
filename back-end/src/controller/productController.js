6ae9c3ba77882eb77b41c66126a937ca598d4206
WalmirLucena <walmirlucena3@gmail.com>
2022-05-04 15:42:20 -0400

    fix: change UserService and middlewares

---
+++ back-end/src/controller/productController.js
@@ -0,0 +1,6 @@
+const ProductService = require('../service/productService');
+
+const listAllProducts = async (_req, res) => {
+    const result = await ProductService.listAllProducts();
+    return res.status(200).json(result);
+};
--- back-end/src/middlewares/newUserValidation.js
+++ back-end/src/middlewares/newUserValidation.js
@@ -4,13 +4,13 @@ const service = require('../service/userService');
 const newUserValidation = async (req, res, next) => {
   const userRegistered = { message: 'User already registered' };
 
-  const newUser = await service.createUser(req.body);
+  const newUser = await service.getOne(req.body.email);
 
   if (!newUser) return (res.status(400).json(userRegistered));
 
   req.body.user = newUser;
 
-next();
+  next();
 };
 
 const userBodyValidation = async (req, res, next) => {
