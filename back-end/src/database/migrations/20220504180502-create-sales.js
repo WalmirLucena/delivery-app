9598d67dd65e624e553e7418e651b41f7b244df7
WalmirLucena <walmirlucena3@gmail.com>
2022-05-04 14:23:59 -0400

    refactor: sales and sales_products migrations

--- back-end/src/database/migrations/20220504180502-create-sales.js
+++ back-end/src/database/migrations/20220504180502-create-sales.js
@@ -55,7 +55,7 @@ module.exports = {
       }
     });
   },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('sales');
-  }
+
+  down: async(queryInterface) => dropTable('sales'),
+
 };
--- back-end/src/database/migrations/20220504182004-create-sales-products.js
+++ back-end/src/database/migrations/20220504182004-create-sales-products.js
@@ -2,32 +2,35 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('sales_products', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
       sale_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        references: {
+          model: 'sales',
+          key: 'id',
+        },
       },
       product_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        references: {
+          model: 'products',
+          key: 'id',
+        },
       },
       quantity: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
+        type: Sequelize.INTEGER,
         allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
       }
     });
   },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('sales_products');
-  }
+
+  down: async(queryInterface) => dropTable('sales_products'),
+
 };
