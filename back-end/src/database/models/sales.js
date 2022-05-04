70b0809f526401ea88b31540e715ce5008d9fd27
WalmirLucena <walmirlucena3@gmail.com>
2022-05-04 14:17:55 -0400

    feat: Create Sale associations

--- back-end/src/database/models/sales.js
+++ back-end/src/database/models/sales.js
@@ -1,8 +1,12 @@
 'use strict';
 const Sale = (sequelize, DataTypes) => {
   const sale = sequelize.define('sales', {
-    user_id: DataTypes.INTEGER,
-    seller_id: DataTypes.INTEGER,
+    user_id: {
+      type: DataTypes.INTEGER, foreignKey: true,
+    },
+    seller_id: {
+      type: DataTypes.INTEGER, foreignKey: true,
+    },
     total_price: DataTypes.INTEGER,
     delivery_address: DataTypes.STRING,
     delivery_number: DataTypes.STRING,
@@ -13,6 +17,16 @@ const Sale = (sequelize, DataTypes) => {
     underscore: true,
     timestamps: false   
   });
+  
+  sale.associate = (models) => {
+    sale.belongsTo(models.User, {
+      foreignKey: 'user_id', as: 'users',
+    });
+    sale.belongsTo(models.User, {
+      foreignKey: 'seller_id', as: 'users',
+    });
+  };
+  
 
   return sale;
 };
