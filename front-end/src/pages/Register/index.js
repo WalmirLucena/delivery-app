1ea0b7c4578caaafd0938e39a4e5fcee72e9a552
saulo <saulolima.c@protonmail.com>
2022-05-06 12:28:59 -0300

    add Toastify- page not loading

--- front-end/src/pages/Register/index.js
+++ front-end/src/pages/Register/index.js
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import { useHistory } from 'react-router-dom';
 import RegisterInput from './RegisterInput';
+import DeliveryContext from '../../context/DeliveryContext';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Register() {
   const [fullName, setFullName] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
+  const { setNewUser } = useContext(DeliveryContext);
+  const history = useHistory();
 
   const validateName = () => {
     const MIN_NAME_LENGTH = 12;
@@ -19,6 +25,23 @@ function Register() {
   const validatePassword = () => {
     const MIN_PASSWORD = 6;
     return password.length > MIN_PASSWORD;
+  };
+
+  const verifyResponse = async (response) => {
+    const { role } = response;
+    if (role === 'register') return '/login';
+    return false;
+  };
+
+  const register = async (event) => {
+    event.preventDefault();
+    const endpoint = '/register';
+    const response = await request(endpoint, { fullName, email, password }, 'post');
+    if (response.message) {
+      toast.error(response.message);
+    }
+    setNewUser(response);
+    history.push(verifyResponse(response));
   };
 
   return (
@@ -44,10 +67,17 @@ function Register() {
           className="register-button"
           data-testid="register-submit-btn"
           disabled={ !validateEmail() || !validatePassword() || !validateName() }
+          onClick={ (event) => register(event) }
           type="submit"
         >
           CADASTRAR
         </button>
+        <div data-testid="common_register__element-invalid_register">
+          <ToastContainer
+            data-testid="common_login__element-invalid-email"
+            position="top-center"
+          />
+        </div>
       </form>
     </div>
   );
