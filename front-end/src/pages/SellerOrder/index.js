c674e51d72316c9ed3f5c2ecfbb68e4ac239a311
Pedro Barreto <pbdeoliveira@gmail.com>
2022-05-09 10:53:14 -0300

    feat: request seller orders

--- front-end/src/pages/SellerOrder/index.js
+++ front-end/src/pages/SellerOrder/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import NavBarSeller from '../../components/NavBarSeller';
 import OrderCard from '../../components/OrderCard';
+import { request } from '../../services/requests';
 
 function SellerOrder() {
+
+  const [orders, setOrders] = React.useState([]);
+ 
+  useEffect( async () => {
+      const endpoint = '/orders';
+      const response = await request(endpoint, {}, 'get');
+      setOrders(response);
+  }, []);
+
+  console.log(orders);
+
   return (
     <>
       <NavBarSeller />
-     <OrderCard />
+      <OrderCard />
     </>
   );
 }
