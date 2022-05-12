c33db81f3807e335f159c4f5278581c565311471
WalmirLucena <walmirlucena3@gmail.com>
2022-05-12 19:06:05 -0400

    remove: exclude saleProductService

--- back-end/src/controller/salesProductController.js
+++
@@ -1,8 +0,0 @@
-const SalesProductsService = require('../service/salesProductService');
-
-const getSalesProduct = async (req, res) => {
-    const result = await SalesProductsService.getSalesProduct(req.body);
-    return res.status(200).json(result);
-};
-
-module.exports = { getSalesProduct };
